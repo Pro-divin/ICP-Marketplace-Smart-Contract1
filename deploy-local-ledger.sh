aa887c44f99b2f3d72e59aac78a571be0c35e4f9
Pro-divin <114354657+Pro-divin@users.noreply.github.com>
2024-11-06 20:22:47 +0200

    Update deploy-local-ledger.sh
    i add the comment on coding to help user to understand the code

--- deploy-local-ledger.sh
+++ deploy-local-ledger.sh
@@ -1,31 +1,43 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash  # Shebang line to specify the script should be run in the Bash shell
 
 # Change the variable to "ic" to deploy the ledger on the mainnet.
-export NETWORK=local
+export NETWORK=local  # Set the network to local for deployment
 
+# Create a new identity named 'minter' with plaintext storage mode
 dfx identity new minter --storage-mode=plaintext
-dfx identity use minter
-export MINTER_ACCOUNT_ID=$(dfx ledger account-id)
-dfx identity use default
-export DEFAULT_ACCOUNT_ID=$(dfx ledger account-id)
-dfx identity use minter
 
+# Switch to the minter identity to perform actions as this user
+dfx identity use minter 
+
+# Get the account ID for the minter identity and store it in a variable
+export MINTER_ACCOUNT_ID=$(dfx ledger account-id)
+
+# Switch back to the default identity
+dfx identity use default 
+
+# Get the account ID for the default identity and store it in a variable
+export DEFAULT_ACCOUNT_ID=$(dfx ledger account-id)
+
+# Switch back to the minter identity again
+dfx identity use minter 
+
+# Deploy the ledger canister with specified settings
 dfx deploy --specified-id ryjl3-tyaaa-aaaaa-aaaba-cai ledger_canister --argument '(variant {
     Init = record {
-      minting_account = "'${MINTER_ACCOUNT_ID}'";
-      initial_values = vec {
+      minting_account = "'${MINTER_ACCOUNT_ID}'";  # Specify the minting account
+      initial_values = vec {  # Set initial values for the ledger
         record {
-          "'${DEFAULT_ACCOUNT_ID}'";
+          "'${DEFAULT_ACCOUNT_ID}'";  # The account that receives initial tokens
           record {
-            e8s = 1000_000_000_000 : nat64;
+            e8s = 1000_000_000_000 : nat64;  # Amount of tokens (in e8s)
           };
         };
       };
-      send_whitelist = vec {};
-      transfer_fee = opt record {
-        e8s = 10_000 : nat64;
+      send_whitelist = vec {};  # No send whitelist for transfers
+      transfer_fee = opt record {  # Set a transfer fee
+        e8s = 10_000 : nat64;  # Amount of the transfer fee (in e8s)
       };
-      token_symbol = opt "LICP";
-      token_name = opt "Local ICP";
+      token_symbol = opt "LICP";  # Set the token symbol
+      token_name = opt "Local ICP";  # Set the token name
     }
   })'
